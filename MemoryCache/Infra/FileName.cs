0679ed215090383aec519c242e8eca7126bfd215
Erik Morais <erik.cqf@outlook.com>
2024-03-11 02:40:11 +0000

    Code Clean up

---
+++ MemoryCache/Infra/FileName.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace MemoryCache.Infra
+//{
+//    public interface IEvictionPolicy<TKey, TValue>
+//    {
+//        void EvictIfNeeded(DataStore<TKey, TValue> dataStore);
+//    }
+//    public class LruEvictionStrategy<TKey, TValue> : IEvictionPolicy<TKey, TValue>
+//    {
+//        public void EvictIfNeeded(DataStore<TKey, TValue> dataStore)
+//        {
+//            while (dataStore.DataHashSet.Count >= dataStore.Options.Capacity)
+//            {
+//                var last = dataStore.DataLinkedList.Last;
+//                if (last != null)
+//                {
+//                    dataStore.DataHashSet.Remove(last.Value);
+//                    dataStore.DataLinkedList.RemoveLast();
+//                    dataStore.Notify(last.Value.keyValuePair.Key, DataStoreEventType.Evicted);
+//                }
+//            }
+//        }
+//    }
+//}
